ff715a4b547f46000fa6420d0e0e1ccfb491ebbb
Wingorman <wingorman1307@gmail.com>
2022-08-13 18:46:31 +0800

    fix isort error2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -11,7 +11,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-from .mixins import MixinsReviewViewSet, ListPostViewSet
+from .mixins import ListPostViewSet, MixinsReviewViewSet
 from reviews.models import Category, Genre, Review, Title
 from users.models import User
 
